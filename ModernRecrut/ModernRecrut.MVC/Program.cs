d26dd1f1a4a1a8e00fb8dcd05316db65554ae893
Nathan Joseph <2145612@cegeplimoilou.ca>
2023-06-17 22:36:55 -0400

    test commit

--- ModernRecrut/ModernRecrut.MVC/Program.cs
+++ ModernRecrut/ModernRecrut.MVC/Program.cs
@@ -6,7 +6,7 @@ using ModernRecrut.MVC.Interfaces;
 using ModernRecrut.MVC.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-// Test commit NJ // Test Jay
+// Test commit NJ // Test Jay // Test Nathan 2
 // Add services to the container.
 builder.Services.AddRazorPages();
 
